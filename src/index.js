e84d23b7425090af94a2bb34df4501f41889ab56
LJ-Fryer <ljf_aus@hotmail.com>
2024-11-10 17:55:28 +1100

    Added AI API integration

--- src/index.js
+++ src/index.js
@@ -1,12 +1,23 @@
-function generatePoem(event) {
-  event.preventDefault();
-
+function showResponse(response) {
   new Typewriter("#poem", {
-    strings: ["Poem here"],
+    strings: response.data.answer,
     autoStart: true,
     cursor: null,
-    delay: 1,
+    delay: "natural",
   });
+}
+
+function generatePoem(event) {
+  event.preventDefault();
+  let userTopic = document.querySelector("#user-topic");
+
+  let apiKey = "2394045tebc4f1e4ea3820407e9abod9";
+  let prompt = `Please generate a four line poem about ${userTopic.value}`;
+  let context =
+    "You are a creative AI Assistant who loves to write poems. You create a new and unique poem every time. Generate your answer in basic HTML. Please don't mention html or HTML in your answer. Sign your poem at the bottom: SheCodes AI in a <strong> element.";
+  let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+
+  axios.get(apiUrl).then(showResponse);
 }
 
 let poemFormElement = document.querySelector("#poem-generator-form");
