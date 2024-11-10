cd1dfa4cb7710ffe649de0679f87ffaa4442cc53
LJ-Fryer <ljf_aus@hotmail.com>
2024-11-10 21:04:53 +1100

    Improved user experience

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ function showResponse(response) {
     strings: response.data.answer,
     autoStart: true,
     cursor: null,
-    delay: "natural",
+    delay: 75,
   });
 }
 
@@ -16,6 +16,10 @@ function generatePoem(event) {
   let context =
     "You are a creative AI Assistant who loves to write poems. You create a new and unique poem every time. Generate your answer in basic HTML. Please don't mention html or HTML in your answer. Sign your poem at the bottom: SheCodes AI in a <strong> element.";
   let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+
+  let poemElement = document.querySelector("#poem");
+  poemElement.classList.remove("hidden");
+  poemElement.innerHTML = `<span class="blink">⏳</ span> Generating a poem about ${userTopic.value} for you`;
 
   axios.get(apiUrl).then(showResponse);
 }
