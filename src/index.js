465bdf5232d35b6cbcfcd741a1a2318b8555850d
LJ-Fryer <ljf_aus@hotmail.com>
2024-11-10 14:04:10 +1100

    Added JS to handle form and Typewriter plugin

---
+++ src/index.js
@@ -0,0 +1,13 @@
+function generatePoem(event) {
+  event.preventDefault();
+
+  new Typewriter("#poem", {
+    strings: ["Poem here"],
+    autoStart: true,
+    cursor: null,
+    delay: 1,
+  });
+}
+
+let poemFormElement = document.querySelector("#poem-generator-form");
+poemFormElement.addEventListener("submit", generatePoem);
